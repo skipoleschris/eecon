d06ed8af9993ebaf392806ea4d1d684c719888ac
Chris Turner <chris@templemore.co.uk>
2013-06-20 08:02:40 +0100

    Removed Scalaz validation and switched to Either with right projection as this is less to explain

--- src/main/scala/com/equalexperts/conference/functional/OrderServiceModule.scala
+++ src/main/scala/com/equalexperts/conference/functional/OrderServiceModule.scala
@@ -1,19 +1,15 @@
 package com.equalexperts.conference.functional
 
-import scalaz._
-import Scalaz._
-
 trait OrderServiceModule extends RepositoryModule with ValidationModule {
 
   object OrderService {
 
     def addAddressToOrder(orderId: OrderId, address: Address[_]): 
-          Validation[ErrorState, Order[Status#InProgress, Requirement#Met, Persisted#Yes]] = 
+          Either[ErrorState, Order[Status#InProgress, Requirement#Met, Persisted#Yes]] = 
       for {
-        validatedAddress <- validate(address)                                                                 // Address[Validated#Yes]
-        currentOrder <- obtainOrder[Status#InProgress, Requirement](orderId)                                  // Order[Status#InProgress, Requirement, Persisted#Yes]
-        updatedOrder <- Order.addAddressToOrder(currentOrder, validatedAddress).success                       // Order[Status#InProgress, Requirement#Met, Persisted#No]
-        storedOrder <- storeOrder(updatedOrder)                                                               // Order[Status#InProgress, Requirement#Met, Persisted#Yes]
+        validatedAddress <- validate(address).right                                                           // Address[Validated#Yes]
+        currentOrder <- obtainOrder[Status#InProgress, Requirement](orderId).right                            // Order[Status#InProgress, Requirement, Persisted#Yes]
+        storedOrder <- storeOrder(Order.addAddressToOrder(currentOrder, validatedAddress)).right              // Order[Status#InProgress, Requirement#Met, Persisted#Yes]
       } yield storedOrder
   }
 }
